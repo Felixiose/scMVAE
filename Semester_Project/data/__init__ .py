fe09837fb3f310a879a8b320b908da5947b14799
Felixiose <vittorifelix@gmail.com>
2022-12-31 14:22:40 +0100

    fixed stuff

--- Semester_Project/data/__init__ .py
+++ Semester_Project/data/__init__ .py
@@ -26,7 +26,6 @@ __all__ = [
 
 def create_dataset(dataset_type: str, *args: Any, **kwargs: Any) -> VaeDataset:
     if dataset_type == "bdp": #TODO ANDREI FIX THE NAMES
-        return scRNADataset(*arg, **kwargs)
-    
+        return scRNADataset(*args, **kwargs)
     else:
         raise ValueError(f"Unknown dataset type: '{dataset_type}'.")
