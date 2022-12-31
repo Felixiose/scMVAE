4ac8d3a002a5847ecf2063588b720e21bc33f8bc
aapsonn <36429896+aapsonn@users.noreply.github.com>
2022-12-31 15:12:00 +0100

    fixing data loading

--- Semester_Project/data/utils.py
+++ Semester_Project/data/utils.py
@@ -3,8 +3,21 @@ from vae_dataset import VaeDataset
 
 
 
-def create_dataset(dataset_type: str, *args: Any, **kwargs: Any) -> VaeDataset:
-    if dataset_type == "bdp": #TODO ANDREI FIX THE NAMES
-        return scRNADataset(*args, **kwargs)
+
+
+def create_dataset(dataset_name: str, *args: Any, **kwargs: Any) -> VaeDataset:
+    data_scheme_json = ".data/" + dataset_name + "/"+ dataset_name +".json"
+    if os.path.exist(data_scheme_json):
+        data_scheme = json.load(open(data_scheme_json, "r"))
+        return scRNADataset(batch_size = BATCH_SIZE,
+                   data_folder = os.path.dirname(data_scheme['data_file']), 
+                   data_file = data_scheme['data_file'], 
+                   label_file = data_scheme['label_file'],
+                   batch_files = data_scheme['batch_files'],
+                   doubles = DOUBLES)
     else:
-        raise ValueError(f"Unknown dataset type: '{dataset_type}'.")
+        raise ValueError(f"Unknown dataset name: '{dataset_name}'.\nMake sure data is in data directory and data scheme json file exists")
+   
+    
+   
+       
