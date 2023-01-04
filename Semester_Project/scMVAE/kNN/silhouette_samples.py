872ea528b0c5b569399a0eaa5f0e7753054957f1
apson <aapsonn@gmail.com>
2023-01-04 16:15:33 +0100

    adding silhouette_samples.py

---
+++ Semester_Project/scMVAE/kNN/silhouette_samples.py
@@ -0,0 +1,291 @@
+import torch
+import math
+import os
+import numpy as np
+import pandas as pd
+import pickle
+from multiprocessing import Pool
+import gc
+
+from ...scMVAE.models import FeedForwardVAE
+import argparse
+from ...scMVAE import utils
+from ...utils import str2bool
+from ...data.utils import create_dataset
+from ...scMVAE.components.component import *
+from scipy.spatial.distance import pdist, squareform
+from sklearn.metrics import silhouette_score
+
+from ..kNN.distances import euclidean_distance, spherical_distance, spherical_projected_gyro_distance, lorentz_distance, poincare_distance
+
+
+parser = argparse.ArgumentParser(description="M-VAE runner.")
+parser.add_argument("--id", type=str, default="id", help="A custom run id to keep track of experiments")
+parser.add_argument("--device", type=str, default="cuda", help="Whether to use cuda or cpu.")
+parser.add_argument("--data", type=str, default="./data", help="Data directory.")
+parser.add_argument("--batch_size", type=int, default=100, help="Batch size.")
+parser.add_argument("--chkpt", type=str, default="", help="Model latent space description.")
+parser.add_argument("--learning_rate", type=float, default=1e-3, help="Learning rate.")
+parser.add_argument("--epochs", type=int, default=500, help="Number of epochs.")
+parser.add_argument("--warmup", type=int, default=100, help="Number of epochs.")
+parser.add_argument("--lookahead", type=int, default=50, help="Number of epochs.")
+parser.add_argument("--model", type=str, default="h2,s2,e2", help="Model latent space description.")
+parser.add_argument("--universal", type=str2bool, default=False, help="Universal training scheme.")
+parser.add_argument("--dataset",
+                    type=str,
+                    default="adipose", 
+                    help="Which dataset to run on. Options: adipose, rgc, celegans")
+parser.add_argument("--h_dim", type=int, default=400, help="Hidden layer dimension.")
+parser.add_argument("--seed", type=int, default=None, help="Random seed.")
+parser.add_argument(
+    "--show_embeddings",
+    type=int,
+    default=0,
+    help="Show embeddings every N test runs. Non-positive values mean never. Only effective if test_every > 0.")
+parser.add_argument(
+    "--export_embeddings",
+    type=int,
+    default=0,
+    help="Export embeddings every N test runs. Non-positive values mean never. Only effective if test_every > 0.")
+parser.add_argument("--test_every",
+                    type=int,
+                    default=0,
+                    help="Test every N epochs during training. Non-positive values mean never.")
+parser.add_argument("--train_statistics",
+                    type=str2bool,
+                    default=False,
+                    help="Show Tensorboard statistics for training.")
+parser.add_argument(
+    "--scalar_parametrization",
+    type=str2bool,
+    default=False,
+    help="Use a spheric covariance matrix (single scalar) if true, or elliptic (diagonal covariance matrix) if "
+    "false.")
+parser.add_argument("--fixed_curvature",
+                    type=str2bool,
+                    default=True,
+                    help="Whether to fix curvatures to (-1, 0, 1).")
+parser.add_argument("--doubles", type=str2bool, default=True, help="Use float32 or float64. Default float32.")
+parser.add_argument("--beta_start", type=float, default=1.0, help="Beta-VAE beginning value.")
+parser.add_argument("--beta_end", type=float, default=1.0, help="Beta-VAE end value.")
+parser.add_argument("--beta_end_epoch", type=int, default=1, help="Beta-VAE end epoch (0 to epochs-1).")
+parser.add_argument("--likelihood_n",
+                    type=int,
+                    default=500,
+                    help="How many samples to use for LL estimation. Value 0 disables LL estimation.")
+parser.add_argument("--batch_file", type=str, default=None, help="Path to a file with labels or batch effects")
+
+args = parser.parse_args()
+
+if args.seed:
+    print("Using pre-set random seed:", args.seed)
+    utils.set_seeds(args.seed)
+
+if not torch.cuda.is_available():
+    args.device = "cpu"
+    print("CUDA is not available.")
+args.device = torch.device(args.device)
+utils.setup_gpu(args.device)
+print("Running on:", args.device, flush=True)
+
+if args.doubles:
+    torch.set_default_dtype(torch.float64)
+else:
+    torch.set_default_dtype(torch.float32)
+
+print(args.model)
+print(args.fixed_curvature)
+COMPONENTS = utils.parse_components(args.model, args.fixed_curvature)
+
+
+def load_model():
+
+    dataset = create_dataset(dataset_type = args.dataset, batch_size=args.batch_size, doubles = args.doubles) 
+
+    model = FeedForwardVAE(h_dim=args.h_dim,
+                        components=COMPONENTS,
+                        dataset=dataset,
+                        scalar_parametrization=args.scalar_parametrization)
+
+    return model, dataset
+    
+
+def create_loader_sequential(model, dataset):
+    
+    model.load_state_dict(torch.load(args.chkpt, map_location=args.device))
+    print("Loaded model: FeedForwardVAE at epoch", args.epochs, "from", args.chkpt)
+    sequential_loader = dataset.create_sequential_loader(args.batch_size)
+    return sequential_loader
+
+def create_X_y(model, loader):
+
+    X_list = []
+    y_list = []
+
+    for batch_idx, (x_mb, y_mb) in enumerate(loader):
+        x_mb = x_mb.to(model.device)
+        reparametrized, concat_z, x_mb_ = model(x_mb)
+        X_list.append(concat_z)
+        y_list.append(y_mb)
+
+    big_X = X_list[0]
+    X_list.pop(0)
+
+    big_y = y_list[0]
+    y_list.pop(0)
+
+    for tensor in X_list:
+        big_X = torch.cat((big_X,tensor))
+
+    for tensor in y_list:
+        big_y = torch.cat((big_y,tensor))
+    
+    return big_X, big_y
+
+def create_manifold_list(model):
+    
+    manifold_list = []
+
+    for i, component in enumerate(model.components):
+
+        manifold_type = type(component)
+        dim = component.dim
+        curvature = float(component.manifold.curvature)
+
+        if manifold_type == UniversalComponent:
+            if curvature > 0:
+                manifold_type = StereographicallyProjectedSphereComponent
+            elif curvature < 0:
+                manifold_type = PoincareComponent
+            else:
+                manifold_type = EuclideanComponent
+
+        manifold = (manifold_type, dim, curvature)
+        manifold_list.append(manifold)
+
+    return manifold_list
+
+
+#FIXME: what about UniversalComponent?
+
+
+model, dataset = load_model()
+sequential_loader = create_loader_sequential(model, dataset)
+X_all, y_all = create_X_y(model, sequential_loader)
+manifold_list = create_manifold_list(model)
+X_all = X_all.detach().numpy().astype(np.float64)
+y_all = y_all.detach().numpy().astype(np.float64).ravel()
+
+def read_factorize_data(filepath):
+        """
+        read a file with a single column, factorize to numerical classes 
+        return factorized data as numpy array
+        """
+        names = pd.read_csv(filepath, header=None)
+        na_filter = np.where(names.notnull().values)
+        fct = pd.factorize(names[0])[0]+1
+        return fct, na_filter
+
+
+def distance(a, b):
+
+    a = torch.from_numpy(a).double()
+    b = torch.from_numpy(b).double()
+
+    distance_sqd = 0
+    counter = 0
+
+    for manifold_type, dim, curvature in manifold_list:
+
+        if curvature != 0:
+            radius = torch.Tensor([1/math.sqrt(abs(curvature))]).double()                                        
+        else:
+            radius = None
+
+        curvature = torch.Tensor([curvature]).double()
+
+        if manifold_type in [EuclideanComponent, ConstantComponent]:
+
+            distance_sqd += euclidean_distance(a[counter:counter+dim], b[counter:counter+dim])**2
+
+        elif manifold_type == SphericalComponent:
+
+            distance_sqd += spherical_distance(a[counter:counter+dim], b[counter:counter+dim], radius)**2
+
+        elif manifold_type == HyperbolicComponent:
+
+            distance_sqd += lorentz_distance(a[counter:counter+dim], b[counter:counter+dim], radius)**2
+
+        elif manifold_type == StereographicallyProjectedSphereComponent:
+
+            distance_sqd += spherical_projected_gyro_distance(a[counter:counter+dim], b[counter:counter+dim], curvature)**2
+
+        elif manifold_type == PoincareComponent:
+
+            distance_sqd += poincare_distance(a[counter:counter+dim], b[counter:counter+dim], radius)**2
+
+        else:
+
+            distance_sqd = "ERROR"
+
+        counter += dim
+
+    return math.sqrt(distance_sqd)
+
+
+def calculate_silhouette(chunk):
+    X, labels = chunk
+    #train
+    dist = squareform(pdist(X, metric=distance))
+     
+    res = silhouette_score(dist, metric = "precomputed", labels=labels)
+    return res
+
+
+na_filter=None
+batch_name = "celltype" 
+
+if args.batch_file is not None:
+    y_all, na_filter = read_factorize_data(args.batch_file)
+    batch_name = os.path.splitext(os.path.basename(args.batch_file))[0]
+
+if X_all.shape[0] != y_all.shape[0]:
+    raise("X_all.shape[0] != y_all.shape[0]")
+    
+if na_filter is not None:
+    X_all = X_all[na_filter[0],]
+    y_all = y_all[na_filter[0],]
+
+n_repicates = 10
+size = 1000
+
+indices = list(range(X_all.shape[0]))
+list_chunks = list()
+
+for i in range(n_repicates):
+    sample = np.random.choice(indices, size=size, replace=False)
+    list_chunks.append((X_all[sample],y_all[sample]))
+
+del X_all
+del y_all
+
+
+gc.collect()
+
+
+pool = Pool(10)
+res_silh = pool.map( 
+    calculate_silhouette, [chunk for chunk in list_chunks ] )
+pool.close()
+
+    
+
+save_path_silh = os.path.dirname(args.chkpt) + "/" +args.id+"_"+batch_name+ "_silhouette_score.tsv"
+
+
+print("Saving results as" +  save_path_silh )
+
+
+with open(save_path_silh, 'w') as tsv:
+        tsv.writelines(        
+        [ f"{args.id}\t{args.dataset}\t{args.model}\t{args.fixed_curvature}\t{args.universal}\t{args.seed}\t{batch_name}\t{silh}\n" for silh in res_silh ])
